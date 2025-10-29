7c747a8014d190d60f237b03ab84ffb8c23a85da
br4d112 <dalejandro011207@hotmail.com>
2025-10-29 18:35:32 -0500

    arreglo swagger documentación

--- src/controllers/userAdmin.controller.js
+++ src/controllers/userAdmin.controller.js
@@ -14,7 +14,7 @@ const adminBoard = (req, res) => {
 
 /**
  * @swagger
- * /sessions:
+ * /user/admin/showLiveSessions:
  *   get:
  *     summary: Lista todas las sesiones activas
  *     description: Obtiene todas las sesiones activas (donde `fecha_fin` es `null`) de la base de datos.
@@ -58,6 +58,44 @@ const showLiveSessions = async (req, res) => {
     res.status(500).send({ message: error.message });
   }
 };
+/**
+ * @swagger
+ * /user/admin/showAllSessions:
+ *   get:
+ *     summary: Lista todas las sesiones
+ *     description: Obtiene todas las sesiones de la base de datos, tanto activas como inactivas.
+ *     tags:
+ *       - Sesiones
+ *     responses:
+ *       200:
+ *         description: Lista de sesiones activas.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     description: ID de la sesión.
+ *                   usuario_id:
+ *                     type: string
+ *                     description: ID del usuario asociado a la sesión.
+ *                   fecha_inicio:
+ *                     type: string
+ *                     format: date-time
+ *                     description: Fecha y hora de inicio de la sesión.
+ *                   fecha_fin:
+ *                     type: string
+ *                     format: date-time
+ *                     description: Fecha y hora de fin de la sesión (null si está activa).
+ *                   token:
+ *                     type: string
+ *                     description: Token asociado a la sesión.
+ *       500:
+ *         description: Error interno del servidor.
+ */
 const showAllSessions = async (req, res) => {
   try {
     const sessions = await userServices.getAllSessions();
